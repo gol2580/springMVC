af9ff70597e15245736632f902c80a1c3532f53e
gol2580 <gol11971197@gmail.com>
2023-06-16 17:16:36 +0900

    서블릿으로 회원관리 웹 애플리케이션 만들기

---
+++ src/main/java/MVC/servlet/domain/member/Member.java
@@ -0,0 +1,18 @@
+package MVC.servlet.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Member {
+    private Long Id;
+    private String username;
+    private int age;
+
+    public Member() {}
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
